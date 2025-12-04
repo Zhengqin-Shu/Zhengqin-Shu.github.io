7c36f164c86f3035b5fda02f62e376666bfa3b11
Zhengqin-Shu <zshu@caltech.edu>
2025-12-04 08:37:06 +0000

    Deploying to gh-pages from @ Zhengqin-Shu/Zhengqin-Shu.github.io@0982c594ea6f0b6a2d535aa7f624e2301e3145d5 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -480,7 +480,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=t9yYw6cAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
