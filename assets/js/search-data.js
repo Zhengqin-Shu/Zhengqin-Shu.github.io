ebf3f5f19d3f29e89882068251435d805115d953
Zhengqin-Shu <zshu@caltech.edu>
2025-12-04 08:27:37 +0000

    Deploying to gh-pages from @ Zhengqin-Shu/Zhengqin-Shu.github.io@a0a3c8fa869634760cb9a9893cbef4bb094319ec 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -473,21 +473,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("mailto:%7A%73%68%75@%63%61%6C%74%65%63%68.%65%64%75", "_blank");
         },
       },{
         id: 'social-scholar',
@@ -495,13 +481,6 @@ ninja.data = [{
         section: 'Socials',
         handler: () => {
           window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
         },
       },{
       id: 'light-theme',
