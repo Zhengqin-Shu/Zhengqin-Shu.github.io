9aee5f6cea43b26dd3b8925207cc307b8eb2d9e2
Zhengqin-Shu <zshu@caltech.edu>
2025-12-06 09:03:21 +0000

    Deploying to gh-pages from @ Zhengqin-Shu/Zhengqin-Shu.github.io@cc30ba496d7df6858705d2dca6fbee452fea0bb9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,18 +23,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "post-",
-        
-          title: "",
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2025/2023-12-10-5-Magic-Gala/";
-          
-        },
-      },{id: "post-aps-dfd-2025",
+        },{id: "post-aps-dfd-2025",
         
           title: "APS DFD 2025",
         
@@ -175,6 +164,17 @@ ninja.data = [{
         handler: () => {
           
             window.location.href = "/blog/2023/New-Year-Gala/";
+          
+        },
+      },{id: "post-the-5th-tsinghua-university-magic-gala-lt-em-gt-into-the-wondrous-way-lt-em-gt",
+        
+          title: "The 5th Tsinghua University Magic Gala: &lt;em&gt; Into the Wondrous Way &lt;/em&gt;",
+        
+        description: "My first director experience",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2023/5-Magic-Gala/";
           
         },
       },{id: "post-tsinghua-39-s-2023-freshman-welcome-gala",
