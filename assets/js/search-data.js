f9fe6e4b8a7e151154496b4ad12b338b00701186
Zhengqin-Shu <zshu@caltech.edu>
2025-12-06 02:07:44 +0000

    Deploying to gh-pages from @ Zhengqin-Shu/Zhengqin-Shu.github.io@fe153c0ba8bfa55a328bebc638c0486cc15e8f09 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -188,17 +188,6 @@ ninja.data = [{
             window.location.href = "/blog/2023/Freshman-Welcome-Gala/";
           
         },
-      },{id: "post-a-post-with-tikzjax",
-        
-          title: "a post with TikZJax",
-        
-        description: "this is what included TikZ code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/tikzjax/";
-          
-        },
       },{id: "post-a-post-with-bibliography",
         
           title: "a post with bibliography",
